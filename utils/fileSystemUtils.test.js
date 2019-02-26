57930e936f09f273b4df1226b44dee3a868dfe77
Jurek <jurek2006@gmail.com>
2019-02-26 10:44:42 +0100

    expect custom matcher toContainObject moved to tests folder

--- utils/fileSystemUtils.test.js
+++ utils/fileSystemUtils.test.js
@@ -1,37 +1,6 @@
 const expect = require('expect');
 const fileSystemUtils = require('./fileSystemUtils');
 
-// custom matcher (https://medium.com/@andrei.pfeiffer/jest-matching-objects-in-array-50fe2f4d6b98)
-// checks if array contains given object
-expect.extend({
-    toContainObject(received, argument) {
-        const pass = this.equals(
-            received,
-            expect.arrayContaining([expect.objectContaining(argument)])
-        );
-
-        if (pass) {
-            return {
-                message: () =>
-                    `expected ${this.utils.printReceived(
-                        received
-                    )} not to contain object ${this.utils.printExpected(
-                        argument
-                    )}`,
-                pass: true
-            };
-        } else {
-            return {
-                message: () =>
-                    `expected ${this.utils.printReceived(
-                        received
-                    )} to contain object ${this.utils.printExpected(argument)}`,
-                pass: false
-            };
-        }
-    }
-});
-
 describe('fileSystemUtils', () => {
     describe('readJsonFile', () => {
         it('read and parse json file', () => {
