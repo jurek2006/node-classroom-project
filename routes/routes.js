feb803e63836d31e7acb7bf4ac3ac54822b98db6
Jurek <jurek2006@gmail.com>
2019-03-11 10:11:04 +0100

    deleted not necessaty route

--- routes/routes.js
+++ routes/routes.js
@@ -9,7 +9,6 @@ router.get("/contact/add", contactController.getAddContact);
 router.post("/contact/save", contactController.postSaveContact);
 
 router.get("/contact/:id", contactController.getContactEdit);
-router.get("/contact/:id/edit", contactController.getContactEdit);
 router.get("/contact/:id/delete", contactController.getContactDelete); //confirm deletion
 router.post("/contact/:id/delete", contactController.postContactDelete); //delete contact
 
