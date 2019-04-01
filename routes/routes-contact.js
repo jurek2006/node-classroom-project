4bc65e3ea02f33fb97ad00ba4850ab2b67075b55
Jurek <jurek2006@gmail.com>
2019-04-01 10:46:43 +0200

    temp disenrolling contacts from its edit

--- routes/routes-contact.js
+++ routes/routes-contact.js
@@ -13,4 +13,7 @@ router.get("/:id", contactController.getContactEdit);
 router.get("/:id/delete", contactController.getContactDelete); //confirm deletion
 router.post("/:id/delete", contactController.postContactDelete); //delete contact
 
+// /contact/ <%= course.id %> /disenroll/ <%= contact.id %> "
+router.get("/:id/disenroll/:contactId", contactController.getDisenroll);
+
 module.exports = router;
